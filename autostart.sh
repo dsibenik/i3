11e65738469f1c23dbfd4c12450249514ff6d683
dsibenik <dsibenik@live.com>
2016-12-12 15:30:33 +0100

    added power menu

--- autostart.sh
+++ autostart.sh
@@ -4,6 +4,7 @@ feh --bg-scale /home/dsibenik/.config/i3/Grey-City-Ariel-Wallpaper.jpg
 guake &
 setxkbmap hr
 antimicro &
+volumeicon &
 sudo mount /dev/sdb1 /mnt/WD-Green-1
 sudo mount /dev/sdb2 /mnt/WD-Green-2
 
