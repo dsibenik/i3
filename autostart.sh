f796feabd28eb301b84570a4a1678f6aba28b7f6
dsibenik <dsibenik@live.com>
2017-09-10 18:18:27 +0200

    orange first commit

--- autostart.sh
+++ autostart.sh
@@ -4,7 +4,8 @@ feh --bg-scale ~/.config/i3/wallpaper.*
 guake &
 setxkbmap hr
 antimicro &
+conky -b -c ~/.config/.conkyrc &
 #volumeicon &
-sudo mount /dev/sdb1 /mnt/WD-Green-1
-sudo mount /dev/sdb2 /mnt/WD-Green-2
+sudo mount /dev/sda1 /mnt/WD-Green-1
+sudo mount /dev/sda2 /mnt/WD-Green-2
 
