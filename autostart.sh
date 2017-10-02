81a98b6c903e115d8d0d4db502c666f343701e1f
Davor Sibenik <dsibenik@live.com>
2017-10-02 18:23:19 +0200

    wallpaper purple

--- autostart.sh
+++ autostart.sh
@@ -1,11 +1,13 @@
 #xrandr --output HDMI-0 --auto --output DVI-D-0 --off
-xrandr --output HDMI-0 --off
+xrandr --output DVI-D-0 --off
 compton --config ~/.config/i3/compton.conf -b
 feh --bg-scale ~/.config/i3/wallpaper.*
 guake &
 setxkbmap hr
 antimicro &
 conky -b -c ~/.config/i3/conky.conf &
+conky -b -c ~/.config/i3/conky2.conf &
+conky -b -c ~/.config/i3/conky3.conf &
 #volumeicon &
 udiskie &
 sudo mount /dev/sda1 /mnt/WD-Green-1
